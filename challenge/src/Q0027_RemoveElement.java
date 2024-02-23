bda3b1e7fd16132e29f8214d66d86fedaac2cf03
Pascal Benimana <pascal.benimana6@gmail.com>
2024-02-23 15:52:47 -0500

    updated

--- challenge/src/Q0027_RemoveElement.java
+++ challenge/src/Q0027_RemoveElement.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Q0027_RemoveElement {
+public class Q0027_RemoveElement {
+    public int removeElement(int[] nums, int val){
+
+        int k = nums.length;
+        for(var i=0; i<k; i++){
+            if(nums[i] == val) {
+                while(nums[k-1] == val)
+                    k--;
+                nums[i] = nums[k-1];
+                nums[k-1] = val;
+                k--;
+            }
+        }
+        return k;
+    }
 }
